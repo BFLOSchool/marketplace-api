c25a636dae8a70d518cea56079cf64afeb41e755
Gregg Mojica <greggmojica@Greggs-Macbook-Pro.local>
2019-02-15 17:48:00 -0500

    chapter 8

--- models/user.js
+++ models/user.js
@@ -25,4 +25,38 @@ var UserSchema = new mongoose.Schema({
   }
 }); 
 
+
+/* pre save hook */
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, null, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+
+/* checkPassword User schema method */
+UserSchema.methods.checkPassword = function (passw, cb) {
+  bcrypt.compare(passw, this.password, function (err, matchFound) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, matchFound);
+  });
+};
+
+
 module.exports = mongoose.model('User', UserSchema);
